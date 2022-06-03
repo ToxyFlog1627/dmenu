0db76d6d2fa854dae38794727381f2625daca57a
ToxyFlog1627 <toxymail1627@gmail.com>
2022-06-03 12:05:38 +0300

    fuzzysearch & fuzzyhighlight

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
 static const unsigned int alpha = 0xffa0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -11,15 +12,17 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { "#eeeeee", "#005577" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
-	[SchemeMid]  = { "#eeeeee", "#770000" },
-};	
-	
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeMid] = { "#eeeeee", "#770000" },
+};
+
 static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel]  = { OPAQUE, alpha },
-        [SchemeOut]  = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+        [SchemeOut] = { OPAQUE, alpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
