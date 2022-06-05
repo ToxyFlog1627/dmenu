10e85fa3b1a2976f823f8feb92925d0b72d7e6b2
ToxyFlog1627 <toxymail1627@gmail.com>
2022-06-05 16:44:56 +0300

    colors

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
-static const unsigned int alpha = 0xffa0;
+static const unsigned int alpha = 0xff40;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack Nerd Font:style=Regular:size=10"
@@ -11,12 +11,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#2277a8" },
-	[SchemeSelHighlight] = { "#ffc978", "#2277a8" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#f8f8f8", "#114499" },
+	[SchemeNorm] = { "#d7e1ff", "#121524" },
+	[SchemeSel] = { "#51576d", "#8caaee" },
+	[SchemeSelHighlight] = { "#eabeff", "#000000" },
+	[SchemeNormHighlight] = { "#eabeff", "#000000" },
+	[SchemeOut] = { "#414559", "#000000" },
+	[SchemeMid] = { "#626880", "#babbf1" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
