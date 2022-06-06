65e69f1048630d6bbd0eb15a9ca65c376e712668
ToxyFlog1627 <toxymail1627@gmail.com>
2022-06-06 14:33:24 +0300

    colors

--- config.h
+++ config.h
@@ -1,19 +1,19 @@
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;
-static const unsigned int alpha = 0xfff0;
+static const unsigned int alpha = 0xffa0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Hack Nerd Font:style=Regular:size=10" };
 static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
 
-static char nrmfg[]    = "#51576d";
+static char nrmfg[]    = "#414559";
 static char nrmbg[]    = "#ca9ee6";
-static char selfg[]    = "#292c3c";
-static char selbg[]    = "#8caaee";
-static char midfg[]    = "#303446";
-static char midbg[]    = "#babbf1";
-static char outfg[]    = "#c6d0f5";
-static char higselfg[] = "#ffffff";
-static char hignrmfg[] = "#e78284";
+static char selfg[]    = "#232634";
+static char selbg[]    = "#efbfff";
+static char midfg[]    = "#292c3c";
+static char midbg[]    = "#e0b0f0";
+static char outfg[]    = "#a5adce";
+static char higselfg[] = "#4455f0";
+static char hignrmfg[] = "#4455f0";
 
 static const char *colors[SchemeLast][2] = {
 	[SchemeNorm]          = { nrmfg, nrmbg },
